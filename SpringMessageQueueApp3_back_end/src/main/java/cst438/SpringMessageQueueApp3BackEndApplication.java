a1e0b94f384f3d5993d4aa31f2c7bca33f705aa5
Christal O'Connell <32943793+Scorp197@users.noreply.github.com>
2021-05-18 17:06:10 -0700

    First commit

---
+++ SpringMessageQueueApp3_back_end/src/main/java/cst438/SpringMessageQueueApp3BackEndApplication.java
@@ -0,0 +1,13 @@
+package cst438;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMessageQueueApp3BackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMessageQueueApp3BackEndApplication.class, args);
+	}
+
+}
---
+++ SpringMessageQueueApp3_back_end/src/test/java/cst438/SpringMessageQueueApp3BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package cst438;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMessageQueueApp3BackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
